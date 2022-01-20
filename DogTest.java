cc326fa95857a2fecb48cfd10ea435f9fb3cef62
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-20 10:38:45 +0530

    CUSTOM EXCEPTION DEFINED

--- DogTest.java
+++ DogTest.java
@@ -5,21 +5,29 @@ public class DogTest {
 		// new keyword performs the allocation of data structure ie name,age
 		try
 		{
-			Dog dog1 = new Dog("Bruno",-22);
+			Dog dog1 = new Dog("Bruno",22);
 			dog1.show();
 		}
-		catch(RuntimeException e) {
+		catch(DogAgeExceededException e) {
 			System.out.println("Problem 1 being handled : "+e.getMessage());
 		}
-		catch(RuntimeException e) {
+		catch(DogAgeInNegativeException e) {
 			System.out.println("Problem 2 being handled : "+e.getMessage());
 		}
 		System.out.println("End main");
 		
 	}
 }
-//class DogAgeExceededException   extends RuntimeException { }
-//class DogAgeInNegativeException extends RuntimeException { }
+class DogAgeExceededException   extends RuntimeException { //isA
+	DogAgeExceededException(String msg) {
+		super(msg);
+	}
+}
+class DogAgeInNegativeException extends RuntimeException { //isA
+	DogAgeInNegativeException(String msg) {
+		super(msg);
+	}
+}
 
 
 class Dog
@@ -32,12 +40,16 @@ class Dog
 		this.name = name;
 		if(age > 14 ) {
 			//System.out.println("Dogs age cannot exceed 14 years...");
-			RuntimeException rte = new RuntimeException("Dogs age cannot exceed 14 years...");
-			throw rte; // throwing this object towards the invoker of this ctor.	
+			//RuntimeException rte = new RuntimeException("Dogs age cannot exceed 14 years...");
+			
+			DogAgeExceededException dogAgeExceedEx = new DogAgeExceededException("Dogs age cannot exceed 14 years...");
+			throw dogAgeExceedEx; // throwing this object towards the invoker of this ctor.	
 		}
 		if(age <= 0 ) {
-			RuntimeException rte = new RuntimeException("Dogs age cannot be in negative or zero..");
-			throw rte; // throwing this object towards the invoker of this ctor.	
+			
+			//RuntimeException rte = new RuntimeException("Dogs age cannot be in negative or zero..");
+			DogAgeInNegativeException dogAgeNegativeEx = new DogAgeInNegativeException("Dogs age cannot be in negative or zero..");
+			throw dogAgeNegativeEx; // throwing this object towards the invoker of this ctor.	
 		}
 		else {
 			System.out.println("Setting the age as "+age);
