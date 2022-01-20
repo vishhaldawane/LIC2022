432b01acf82653e251e57a3e78793003766c5e74
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-20 11:08:28 +0530

    checked vs unchecked implementation

--- DogTest.java
+++ DogTest.java
@@ -5,8 +5,9 @@ public class DogTest {
 		// new keyword performs the allocation of data structure ie name,age
 		try
 		{
-			Dog dog1 = new Dog("Bruno",22);
+			Dog dog1 = new Dog("Bruno",5);
 			dog1.show();
+			dog1.roam();
 		}
 		catch(DogAgeExceededException e) {
 			System.out.println("Problem 1 being handled : "+e.getMessage());
@@ -14,34 +15,69 @@ public class DogTest {
 		catch(DogAgeInNegativeException e) {
 			System.out.println("Problem 2 being handled : "+e.getMessage());
 		}
+		catch(BarkingException b) {
+			System.out.println("Problem 3 being handled : "+b.getMessage()+" what happened...why r u barking....????");
+		}
 		System.out.println("End main");
 		
 	}
 }
-class DogAgeExceededException   extends RuntimeException { //isA
+
+//InsufficientFundsException - withdraw()
+//AccountNotFoundException - while seaching an account
+//DepositExceedIncomeTaxException - while depositing more than the income tax level
+//PayeeNotFoundException - while fund transfer
+
+
+
+/*
+  unchecked type need not be mentioned in the throws clause of the Dog's
+  constructor or any method 
+  
+class DogAgeExceededException   extends RuntimeException { //isA - unchecked
 	DogAgeExceededException(String msg) {
 		super(msg);
 	}
 }
-class DogAgeInNegativeException extends RuntimeException { //isA
+class DogAgeInNegativeException extends RuntimeException { //isA - unchecked
+	DogAgeInNegativeException(String msg) {
+		super(msg);
+	}
+}
+*/
+
+// checked type must be mentioned in the throws clause of the Dog's
+// constructor or any method 
+
+class DogAgeExceededException   extends Exception { //isA - checked
+	DogAgeExceededException(String msg) {
+		super(msg);
+	}
+}
+class DogAgeInNegativeException extends Exception { //isA - checked
 	DogAgeInNegativeException(String msg) {
 		super(msg);
 	}
 }
 
+class BarkingException extends RuntimeException { //isA  - unchecked
+	BarkingException(String str) {
+		super(str);
+	}
+}
 
 class Dog
 {
 	private String name;
 	private int age;
 	
-	Dog(String name, int age) {
+	Dog(String name, int age) throws DogAgeExceededException, DogAgeInNegativeException 
+	{
 		System.out.println("=>Dog(String,int) constructor...started..");
 		this.name = name;
 		if(age > 14 ) {
 			//System.out.println("Dogs age cannot exceed 14 years...");
 			//RuntimeException rte = new RuntimeException("Dogs age cannot exceed 14 years...");
-			
 			DogAgeExceededException dogAgeExceedEx = new DogAgeExceededException("Dogs age cannot exceed 14 years...");
 			throw dogAgeExceedEx; // throwing this object towards the invoker of this ctor.	
 		}
@@ -59,6 +95,17 @@ class Dog
 
 	}
 	
+	void roam() 
+	{
+		for(int i=1;i<=15;i++) {
+			System.out.println(name+" Dog is roaming..."+i+" minute over...");
+			double value = Math.random(); // generate number from 0.0 to 1.0
+			if(value >0.75 && value < 0.80) {
+				BarkingException barkEx = new BarkingException("Dog is barking...bhu bhu bho..bho.");
+				throw barkEx;
+			}
+		}
+	}
 	void show() {
 		System.out.println("hi "+name+" your age is "+age);
 	}
